ea96464be26655768217a29e88aa358ec90136f5
Hidélgio Novela <hidelgionovela@gmail.com>
2022-08-07 22:47:21 +0200

    Create FooterComponent.vue

---
+++ src/components/base/FooterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-footer elevated>
+        <q-toolbar>
+          <q-toolbar-title>Footer</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HeaderComponent'
+})
+</script>
+
+<style scoped>
+
+</style>
