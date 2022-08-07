d829d39e56f3727d9e4e3f0083f74adeb956a712
Hidélgio Novela <hidelgionovela@gmail.com>
2022-08-07 22:47:28 +0200

    Create HeaderComponent.vue

---
+++ src/components/base/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-header elevated>
+        <q-toolbar>
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+
+          <q-toolbar-title>Quasar Framework</q-toolbar-title>
+
+          <q-btn flat round dense icon="whatshot" />
+        </q-toolbar>
+      </q-header>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HeaderComponent'
+})
+</script>
+
+<style scoped>
+
+</style>
