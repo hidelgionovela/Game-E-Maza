3ff762efe0a0e0db703121a37e89f2ae02a47fa1
Hidélgio Novela <hidelgionovela@gmail.com>
2022-08-08 22:02:31 +0200

    style : desenhanho o estilo do footer flutuante

--- src/components/base/FooterComponent.vue
+++ src/components/base/FooterComponent.vue
@@ -1,16 +1,37 @@
 <template>
-  <q-footer elevated>
-        <q-toolbar>
-          <q-toolbar-title>Footer</q-toolbar-title>
-        </q-toolbar>
+  <q-footer elevated class="bg-transparent">
+        <div class="q-pa-sm" >
+        <q-tabs
+        style="border-radius : 25px"
+        v-model="tab"
+        indicator-color="transparent"
+        active-color="primary"
+        class="bg-white text-grey shadow-2"
+      >
+        <q-tab name="mails" icon="mail" />
+        <q-tab name="alarms1" icon="alarm" />
+        <q-tab name="alarms2" icon="alarm" />
+        <q-tab name="alarms3" icon="alarm" />
+        <q-tab name="movies" icon="movie" />
+      </q-tabs>
+      </div>
       </q-footer>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+
 
 export default defineComponent({
-  name: 'HeaderComponent'
+   name: 'FooterComponent',
+   
+    setup() {
+        const tab = ref('mail')
+        return{
+                tab
+        }
+    }
+    
 })
 </script>
 
