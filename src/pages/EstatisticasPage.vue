4de230d4158c1cc5566e8055b06624f1c1e6e955
Hidélgio Novela <hidelgionovela@gmail.com>
2022-08-09 19:39:10 +0200

    Create EstatisticasPage.vue

---
+++ src/pages/EstatisticasPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-md">
+ 
+    <div>Estatisticas | Page</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EstatisticasPage'
+})
+</script>
