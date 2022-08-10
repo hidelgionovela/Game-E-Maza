5eace623ed47cd1bb15d728158a5db804c77aae0
Hidélgio Novela <hidelgionovela@gmail.com>
2022-08-10 22:59:19 +0200

    style: adicionandonovos icons no footer

--- src/components/base/FooterComponent.vue
+++ src/components/base/FooterComponent.vue
@@ -1,18 +1,18 @@
 <template>
-  <q-footer elevated class="bg-transparent">
+  <q-footer class="bg-transparent">
         <div class="q-pa-sm" >
         <q-tabs
-        style="border-radius : 25px"
+        style="border-radius : 29px"
         v-model="tab"
         indicator-color="transparent"
         active-color="primary"
         class="bg-white text-grey shadow-2"
       >
-        <q-route-tab to="/" name="mails" icon="mail" />
-        <q-route-tab to="/estatistica" name="estatistica" icon="alarm" />
-        <q-route-tab to="/pontos" name="alarms2" icon="alarm" />
-        <q-route-tab to="/ranking" name="alarms3" icon="alarm" />
-        <q-route-tab to="/user" name="movies" icon="movie" />
+        <q-route-tab to="/" name="mails" icon="las la-tv" />
+        <q-route-tab to="/estatistica" name="estatistica" icon="eva-pie-chart-outline" />
+        <q-route-tab to="/pontos" name="alarms2" icon="las la-trophy" />
+        <q-route-tab to="/ranking" name="alarms3" icon="eva-star-outline" />
+        <q-route-tab to="/user" name="movies" icon="las la-user" />
       </q-tabs>
       </div>
       </q-footer>
