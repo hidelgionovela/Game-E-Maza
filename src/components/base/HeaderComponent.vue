00cf6096d007c6a5986e680dabbf4a73bd6987a8
Hidélgio Novela <hidelgionovela@gmail.com>
2022-08-11 04:22:40 +0200

    Update HeaderComponent.vue

--- src/components/base/HeaderComponent.vue
+++ src/components/base/HeaderComponent.vue
@@ -1,15 +1,18 @@
 <template>
         <q-header class="bg-white text-primary">
                 <q-toolbar>
-                        <q-btn flat round  icon="menu" class="" />
+                        <q-btn flat round icon="menu" class="" />
 
                         <q-toolbar-title>
                                 <div class=" text-center">
-                                <div class="text-dark text-bold" style="font-size: 12px; margin-top: 5px;">Caloiro</div>
-                                <div>
-                                        <q-linear-progress rounded size="13px" :value="progress" color="primary" class="q-mt-xs" />
-                                </div>
-                                <div style="font-size: 12px">300 pts <span class="text-grey-9 text-bold"> / 1000 pts</span> </div>
+                                        <div class="text-dark text-bold" style="font-size: 12px; margin-top: 5px;">
+                                                Caloiro</div>
+                                        <div>
+                                                <q-linear-progress rounded size="13px" :value="progress" color="primary"
+                                                        class="q-mt-xs" />
+                                        </div>
+                                        <div style="font-size: 12px">300 pts <span class="text-grey-9 text-bold"> / 1000
+                                                        pts</span> </div>
                                 </div>
                         </q-toolbar-title>
 
@@ -27,15 +30,15 @@ import { defineComponent, ref } from 'vue'
 
 
 export default defineComponent({
-   name: 'HeaderComponent',
-   
-    setup() {
-        const progress = ref(0.4)
-        return {
-                progress
+        name: 'HeaderComponent',
+
+        setup() {
+                const progress = ref(0.4)
+                return {
+                        progress
+                }
         }
-    }
-    
+
 })
 
 
